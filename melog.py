77313a2997ecf21da5f863e527f89e3daea5a381
unknown <roddac@AS017710.synchrotron.org.au>
2014-10-03 19:31:22 +1000

    Basic mysql commands running

--- melog.py
+++ melog.py
@@ -15,14 +15,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@%s/%s' % (username,password,host,db_name)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@host:port/db_name'
 db = SQLAlchemy(app)
-db.Model.metadata.reflect[db.engine]
+#db.Model.metadata.reflect[db.engine]
 
 class ElogGroupData(db.Model):
 
     __tablename__ = 'elog_group_data'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text)
+    group_id = db.Column(db.Integer, primary_key=True)
+    group_title = db.Column(db.Text)
     sort = db.Column(db.Integer)
     private = db.Column(db.Integer)
 
@@ -31,20 +31,21 @@ class ElogData(db.Model):
     __tablename__ = 'elog_data'
 
 # 	entry_id	title	author	created	text	severity_id	beam_mode_id	parent_id	read_only	comment
-    id = db.Column(db.Integer, primary_key=True)
+    entry_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text)
     author = db.Column(db.Text)
-    date = db.Column(db.DateTime)
-    content = db.Column(db.Text)
-    severity = db.Column(db.Integer)
-    beam_mode = db.column(db.Integer)
-    parent = db.column(db.Integer)
-    read_only = db.column(db.Integer)
-    comment = db.column(db.Integer)
+    created = db.Column(db.DateTime)
+    text = db.Column(db.Text)
+    severity_id = db.Column(db.Integer)
+    beam_mode_id = db.Column(db.Integer)
+    parent_id = db.Column(db.Integer)
+    read_only = db.Column(db.Integer)
+    comment = db.Column(db.Integer)
 
 @app.route('/')
 def meLog():
-    return render_template("index.html")
+    groups = ElogGroupData.query.all()
+    return render_template("index.html", groups=groups)
 
 
 if __name__ == '__main__':
