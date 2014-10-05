4b96fc3bd83eb4757c692a7ded327d26d51719fd
unknown <roddac@AS017710.synchrotron.org.au>
2014-10-05 14:54:27 +1100

    Basic display of elog entries

--- melog/views.py
+++ melog/views.py
@@ -3,13 +3,15 @@ from melog import app
 from melog.models import ElogGroupData,ElogData
 from datetime import datetime
 
-@app.route('/<int:year>/<int:month>/<int:day>')
-@app.route('/',defaults={'year':None,'month':None,'day':None})
-def meLog(year,month,day):
-    groups = ElogGroupData.query.filter(ElogGroupData.private == 0)
+@app.route('/<group>/<int:year>/<int:month>/<int:day>/')
+@app.route('/<group>/',defaults={'year':None,'month':None,'day':None})
+@app.route('/',defaults={'group':None,'year':None,'month':None,'day':None})
+def meLog(group,year,month,day):
+    groups = ElogGroupData.query.filter(ElogGroupData.private == 0).order_by(ElogGroupData.sort)
+    eLog = ElogData.query.limit(5).all()
     time = datetime.now().time().strftime("%H:%M:%S")
     date = datetime.now().date()
-    return render_template("index.html", timestamp=time, datestamp=date, groups=groups)
+    return render_template("index.html", timestamp=time, datestamp=date, groups=groups, elogEntry=eLog)
     #return render_template("index.html")
 
 @app.route('/test/')
