d645116b738ea4ed64171e37aca1931d20c2d3b7
Cameron Rodda <cameron.rodda@synchrotron.org.au>
2014-10-16 06:20:24 +1100

    added sol_users model to link users to groups

--- melog/models.py
+++ melog/models.py
@@ -25,3 +25,27 @@ class ElogData(db.Model):
     parent_id = db.Column(db.Integer)
     read_only = db.Column(db.Integer)
     comment = db.Column(db.Integer)
+
+class SolUsers(db.model):
+
+    __tablename__ = 'sol_users'
+
+#	id	name	username	email	password	usertype	gid	registerDate	lastvisitDate	elog_hide_important	elog_collapse_all	elog_entries_per_page	site_nickname	elog_simple_editor	elog_shadow_boxer	elog_show_keywords	elog_date_format	guest
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.VARCHAR(255))
+    username = db.Column(db.VARCHAR(150))
+    email = db.Column(db.VARCHAR(100))
+    password = db.Column(db.VARCHAR(100))
+    usertype = db.Column(db.VARCHAR(25))
+    gid = db.Column(db.Integer)
+    registerDate = db.Column(db.DateTime)
+    lastvisitDate = db.Column(db.DateTime)
+    elog_hide_important = db.Column(db.Integer)
+    elog_collapse_all = db.Column(db.Integer)
+    elog_entries_per_page = db.Column(db.Integer)
+    site_nickname = db.Column(db.VARCHAR(255))
+    elog_simple_editor = db.Column(db.Integer)
+    elog_shadow_boxer = db.Column(db.Integer)
+    elog_show_keywords = db.Column(db.Integer)
+    elog_date_format = db.Column(db.Text)
+    guest = db.Column(db.Integer)
