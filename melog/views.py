6d0177fec53900fb5f4d82f4e13295c8978f281a
Cameron Rodda <cameron.rodda@synchrotron.org.au>
2014-10-18 01:08:13 +1100

    updated group dropdown to reflect selected urlGroup

--- melog/views.py
+++ melog/views.py
@@ -10,10 +10,10 @@ import ldap
 #https://pythonhosted.org/Flask-SQLAlchemy/index.html
 #http://flask.pocoo.org/docs/0.10/patterns/sqlalchemy/
 
-@app.route('/<group>/<int:year>/<int:month>/<int:day>/')
-@app.route('/<group>/',defaults={'year':None,'month':None,'day':None})
+@app.route('/<urlGroup>/<int:year>/<int:month>/<int:day>/')
+@app.route('/<urlGroup>/',defaults={'year':None,'month':None,'day':None})
 @app.route('/',defaults={'group':None,'year':None,'month':None,'day':None})
-def meLog(group,year,month,day):
+def meLog(urlGroup,year,month,day):
     if 'username' not in session:
         return redirect(url_for('Login'))
 
@@ -27,17 +27,22 @@ def meLog(group,year,month,day):
     if request.args.get('d'):
         day = request.args.get('d')
 
-    if group != None:
-        urlGroup = ElogGroupData.query.filter(ElogGroupData.urlName == group).first_or_404()
+    if urlGroup != None:
+        tmpGroup = ElogGroupData.query.filter(ElogGroupData.urlName == urlGroup).first_or_404()
+        group = tmpGroup.group_title
         #app.logger.debug(urlGroup.group_title)
     # else load the default
+    else:
+        group = session['group']
+        urlGroup = ElogGroupData.query.filter(ElogGroupData.group_title == group).first_or_404()
+
     time = datetime.now().time().strftime("%H:%M:%S")
     date = datetime.now().date()
 
     groups = ElogGroupData.query.filter(ElogGroupData.private == 0).order_by(ElogGroupData.sort)
     eLog = ElogData.query.filter(ElogData.created > date).from_self().order_by(ElogData.created)
 
-    return render_template("index.html", timestamp=time, datestamp=date, groups=groups, elogEntry=eLog)
+    return render_template("index.html", timestamp=time, datestamp=date, groups=groups, default_group=group, elogEntry=eLog)
     #return render_template("index.html")
 
 @app.route('/test/')
@@ -57,22 +62,24 @@ def Login():
         ldap_dn = data['ldap_dn']
         ldap_sx = data['ldap_sx']
         connect = ldap.initialize(ldap_server)
-        #success = False
+
+        #attempt bind to ldap, if it fails, then try again
         try:
             connect.simple_bind_s(username+ldap_sx,password)
-            #success = True
 
         except ldap.LDAPError,e:
             #Failed login unbind and return to login screen
             connect.unbind_s()
             return render_template("login.html", error=e)
 
-        #if success:
-        session['username'] = username
-
+        #if success
         tmp_group = SolUsers.query.filter(SolUsers.username == username).first_or_404()
         default_group = ElogGroupData.query.filter(ElogGroupData.group_id == tmp_group.gid).first_or_404()
         #app.logger.debug(default_group.urlName)
+
+        #assign session variables
+        session['username'] = username
+        session['group'] = default_group.group_title
 
         return redirect("/%s" % default_group.urlName)
             #return render_template("index.html")
