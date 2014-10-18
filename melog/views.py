ce5f2a2de97ad831ac131fca0e44542ed7e15f8e
Cameron Rodda <cameron.rodda@synchrotron.org.au>
2014-10-19 04:30:30 +1100

    full date url navigation implemented - options for /year, /year/month, /year/month/day

--- melog/views.py
+++ melog/views.py
@@ -2,6 +2,7 @@ from flask import render_template,session,redirect,url_for,request
 from melog import app
 from config import data
 from melog.models import ElogGroupData,ElogGroups,ElogData,SolUsers
+from sqlalchemy.sql import extract
 from datetime import datetime
 import ldap
 
@@ -11,6 +12,8 @@ import ldap
 #http://flask.pocoo.org/docs/0.10/patterns/sqlalchemy/
 
 @app.route('/<urlGroup>/<int:year>/<int:month>/<int:day>/')
+@app.route('/<urlGroup>/<int:year>/<int:month>/',defaults={'day':None})
+@app.route('/<urlGroup>/<int:year>/',defaults={'month':None,'day':None})
 @app.route('/<urlGroup>/',defaults={'year':None,'month':None,'day':None})
 @app.route('/',defaults={'urlGroup':None,'year':None,'month':None,'day':None})
 def meLog(urlGroup,year,month,day):
@@ -30,18 +33,30 @@ def meLog(urlGroup,year,month,day):
     # get the current date and time
     time = datetime.now().time().strftime("%H:%M:%S")
     date = datetime.now().date()
+    defaultDate = False
+    defaultYear = False
+    defaultMonth = False
+    defaultDay = False
+
     # If year not specified use current
     if not year:
         year = date.year
+        defaultDate = True
+        defaultYear = True
 
     # If month not specified use current
     if not month:
         month = date.month
+        defaultDate = True
+        defaultMonth = True
 
     # If day not specified use current
     if not day:
         day = date.day
+        defaultDate = True
+        defaultDay = True
 
+    #use the urlGroup var if specified
     if urlGroup != None:
         tmpGroup = ElogGroupData.query.filter(ElogGroupData.urlName == urlGroup).first_or_404()
         group = tmpGroup.group_title
@@ -56,12 +71,37 @@ def meLog(urlGroup,year,month,day):
     #Get a list of available groups for select menu display
     groups = ElogGroupData.query.filter(ElogGroupData.private == 0).order_by(ElogGroupData.sort)
 
-    #Get the relevent log entries for group(if no date specified - then todays date)
-
-    eLogJoin = ElogData.query.join(ElogGroups,(ElogGroups.entry_id == ElogData.entry_id))\
-                             .filter(ElogData.created > date,ElogGroups.group_id == groupNum)\
-                             .from_self()\
-                             .order_by(ElogData.created)
+    if defaultDate:
+        if defaultYear:
+            #Get the relevent log entries for group(if no date specified - then todays date as default)
+            eLogJoin = ElogData.query.join(ElogGroups,(ElogGroups.entry_id == ElogData.entry_id)) \
+                                    .filter(ElogData.created > date,ElogGroups.group_id == groupNum) \
+                                    .from_self() \
+                                    .order_by(ElogData.created.desc())
+        elif defaultMonth:
+            #use supplied year var and defaults for the rest
+            eLogJoin = ElogData.query.join(ElogGroups,(ElogGroups.entry_id == ElogData.entry_id)) \
+                                    .filter(ElogGroups.group_id == groupNum) \
+                                    .filter(extract('year',ElogData.created) == year) \
+                                    .from_self() \
+                                    .order_by(ElogData.created.desc())
+        elif defaultDay:
+            #use supplied year and month vars and defaults for the rest
+            eLogJoin = ElogData.query.join(ElogGroups,(ElogGroups.entry_id == ElogData.entry_id)) \
+                                    .filter(ElogGroups.group_id == groupNum) \
+                                    .filter(extract('year',ElogData.created) == year) \
+                                    .filter(extract('month',ElogData.created) == month) \
+                                    .from_self() \
+                                    .order_by(ElogData.created.desc())
+    else:
+        #get the entries based on the supplied date information
+        eLogJoin = ElogData.query.join(ElogGroups,(ElogGroups.entry_id == ElogData.entry_id)) \
+                                .filter(ElogGroups.group_id == groupNum) \
+                                .filter(extract('year',ElogData.created) == year) \
+                                .filter(extract('month',ElogData.created) == month) \
+                                .filter(extract('day',ElogData.created) == day) \
+                                .from_self() \
+                                .order_by(ElogData.created.desc())
 
     return render_template("index.html", timestamp=time, datestamp=date, groups=groups, default_group=group, elogEntry=eLogJoin)
     #return render_template("index.html")
